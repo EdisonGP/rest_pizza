49f11421c32ee00fc40eb18cfd6e22cb7dd2cd09
Edison Guaichico <guaichicoeison@gmail.com>
2022-12-15 07:54:46 -0800

    modificando los demás parámetros de la bbdd

--- cnn.js
+++ cnn.js
@@ -1,11 +1,11 @@
 const pgPromise = require('pg-promise')
-require('dotenv').config()
+const {DB_HOST,DB_PORT,DB_NAME,DB_USER,DB_PASSWORD}=require("./config")
 const config={
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    database:process.env.DB_NAME,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD
+    host:DB_HOST,
+    port:DB_PORT,
+    database:DB_NAME,
+    user:DB_USER,
+    password:DB_PASSWORD
 }
 const pgp = pgPromise({})
 const db = pgp(config)
